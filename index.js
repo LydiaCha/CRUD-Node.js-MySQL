c27a06e82bd4c1761ed7ac2fe18cbc0e4e68614d
Lydia <lydia.chatziioannou@gmail.com>
2020-08-11 15:53:22 +0100

    images

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const cors = require('cors');
 const mysql = require('mysql');
 const { query } = require('express');
 
+const multer = require('multer');
+const upload = multer({dest: 'public/images'}); // uploaded article image here
+
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+
 const app = express();
 
 //all queries go here
@@ -30,11 +36,15 @@ app.get('/', (req, res) => {
     res.send('go to /articles to see articles')
 });
 
-//routes
-app.get('/articles/add', (req, res) => {
-const { title, content } = req.query; //fields from db
-const INSERT_ARTICLES_QUERY = `INSERT INTO articles (title, content) VALUES(?, ?)`;
-connection.query(INSERT_ARTICLES_QUERY, [title, content], (err, results) => {
+//ROUTES
+
+//Add new article
+app.use('/image', express.static('public/images'));
+app.get('/articles/add', upload.single('image'), (req, res) => {
+
+const { title, content, image } = req.query; //fields from db
+const INSERT_ARTICLES_QUERY = `INSERT INTO articles (title, content, image) VALUES(?, ?, ?)`;
+connection.query(INSERT_ARTICLES_QUERY, [title, content, image], (err, results) => {
     if(err) {
         return res.send(err)
     }
@@ -44,6 +54,7 @@ connection.query(INSERT_ARTICLES_QUERY, [title, content], (err, results) => {
 });
 });
 
+//View all articles
 app.get('/articles', (req, res) => {
     connection.query(SELECT_ALL_ARTICLES_QUERY, (err, results) => {
         if(err) {
@@ -56,6 +67,33 @@ app.get('/articles', (req, res) => {
         }
             });
 });
+
+
+// Cloudinary for images
+// cloudinary.config({
+//     cloud_name: process.env.CLOUD_NAME,
+//     api_key: process.env.API_KEY,
+//     api_secret: process.env.API_SECRET
+//     });
+//     const storage = cloudinaryStorage({
+//     cloudinary: cloudinary,
+//     folder: "demo",
+//     allowedFormats: ["jpg", "png"],
+//     transformation: [{ width: 500, height: 500, crop: "limit" }]
+//     });
+//     const parser = multer({ storage: storage });
+
+//     // POST route for Cloudinary
+//     app.post('/api/images', parser.single("image"), (req, res) => {
+//         console.log(req.file) // to see what is returned to you
+//         const image = {};
+//         image.url = req.file.url;
+//         image.id = req.file.public_id;
+//         Image.create(image) // save image information in database
+//           .then(newImage => res.json(newImage))
+//           .catch(err => console.log(err));
+//       });
+
 
 app.listen(4000, () => {
     console.log('Articles server listening on port 4000')
